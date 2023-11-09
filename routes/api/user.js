9cb8d2da14f4fd88044e277309164cd6cf8cdcf0
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-11-09 19:06:15 +0200

    first commit

---
+++ routes/api/user.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { validateBody, authorization } = require('../../middlewares');
+const { schemas } = require('../../models/user');
+// const upload = require('../../middlewares/upload');
+
+const router = express.Router();
+const ctrl = require('../../controllers/user')
+
+router.post('/register', validateBody(schemas.userJoiSchema), ctrl.register);
+
+router.post('/login', validateBody(schemas.userJoiSchema), ctrl.login)
+
+router.get("/current", authorization, ctrl.getCurrent);
+
+router.post('/logout', authorization, ctrl.logout);
+
+router.patch('/', authorization, validateBody(schemas.validateSubscription), ctrl.updateSubscription);
+
+router.get('/verify/:verificationToken', ctrl.verifyEmail);
+
+router.post('/verify/', validateBody(schemas.emailSchema), ctrl.resendVerifyEmail);
+
+module.exports = router;
